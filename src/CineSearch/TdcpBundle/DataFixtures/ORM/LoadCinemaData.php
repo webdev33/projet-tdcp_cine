78e892866b62279b8cc6de8dabaa3fd482ec9e02
inso-61 <damien.galicher@free.fr>
2015-11-03 11:46:23 +0100

    update LoadCinemaData

--- src/CineSearch/TdcpBundle/DataFixtures/ORM/LoadCinemaData.php
+++ src/CineSearch/TdcpBundle/DataFixtures/ORM/LoadCinemaData.php
@@ -18,6 +18,42 @@ class LoadCinemaData implements FixtureInterface
         $cinema1->setLat(48.322639);
         $cinema1->setLng(0.818083);
 
+        $cinema2 = new Cinema();
+        $cinema2->setNom('Cinéma Les Enfants du Paradis');
+        $cinema2->setAdresse('11 pl Porte St Michel, 28000 CHARTRES');
+        $cinema2->setLat(48.442194);
+        $cinema2->setLng(1.489047);
+
+        $cinema3 = new Cinema();
+        $cinema3->setNom('Cinéma Normandie');
+        $cinema3->setAdresse('6 r Desire Roussel, 28260 ANET');
+        $cinema3->setLat(48.857349);
+        $cinema3->setLng(1.439172);
+
+        $cinema4 = new Cinema();
+        $cinema4->setNom('CINECENTRE SNCD (SARL)');
+        $cinema4->setAdresse('5 pl Champ de Foire, 28100 DREUX');
+        $cinema4->setLat(48.731456);
+        $cinema4->setLng(1.360632);
+
+        $cinema5 = new Cinema();
+        $cinema5->setNom('Cinéma L\'Ambiance');
+        $cinema5->setAdresse('10 r Flandres Dunkerque, 28250 SENONCHES');
+        $cinema5->setLat(48.561630);
+        $cinema5->setLng(1.032991);
+
+        $cinema6 = new Cinema();
+        $cinema6->setNom('Cinéma Le Dunois');
+        $cinema6->setAdresse('24 r Lambert Licors, 28200 CHATEAUDUN');
+        $cinema6->setLat(48.071218);
+        $cinema6->setLng(1.331588);
+
+        $cinema7 = new Cinema();
+        $cinema7->setNom('Les Prairiales');
+        $cinema7->setAdresse('Avenue de la Prairie 28230 EPERNON');
+        $cinema7->setLat(48.604807);
+        $cinema7->setLng(1.675288);
+
         $manager->persist($cinema1);
         $manager->flush();
     }
